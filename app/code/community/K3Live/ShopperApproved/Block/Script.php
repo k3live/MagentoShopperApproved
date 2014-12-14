8f6efd0bff5e2272ca2391384bed35c408c18581
Kevin M <kmcgregor@gmail.com>
2014-12-13 22:11:21 -0800

    Updated to Version 1.1
    Now automatically fills out customer name, email, and order id.

--- app/code/community/K3Live/ShopperApproved/Block/Script.php
+++ app/code/community/K3Live/ShopperApproved/Block/Script.php
@@ -1,5 +1,13 @@
 <?php
-class K3Live_ShopperApproved_Block_Script extends Mage_Core_Block_Template
+/**
+ * ShopperApproved Module for Magento 
+ * @package     K3Live_ShopperApproved
+ * @author      K3Live (http://www.k3live.com/)
+ * @copyright   Copyright (c) 2014 K3Live (http://www.k3live.com/)
+ * @license     Open Software License
+ */
+ 
+class ShopperApproved_RatingsReviews_Block_Script extends Mage_Core_Block_Template
 {
     /*
      * Constructor method
@@ -11,8 +19,6 @@ class K3Live_ShopperApproved_Block_Script extends Mage_Core_Block_Template
     public function _construct()
     {
         parent::_construct();
-
-        // Decide whether the ShopperApproved Plugin has been activated
         $website_id = Mage::helper('shopperapproved')->getAccountId();
         if(Mage::helper('shopperapproved')->getEnabled() == true && !empty($website_id)) {
             $this->setTemplate('shopperapproved/script.phtml');
@@ -20,7 +26,7 @@ class K3Live_ShopperApproved_Block_Script extends Mage_Core_Block_Template
     }
 
     /*
-     * Helper method to get data to show in ShopperApproved
+     * Helper method
      *
      * @access public
      * @param string $key
@@ -31,9 +37,29 @@ class K3Live_ShopperApproved_Block_Script extends Mage_Core_Block_Template
         static $data;
         if(empty($data)) {
             $data = array(
-                'account' => Mage::helper('shopperapproved')->getAccountId(), // ShopperApproved Account ID
-                'enabled' => Mage::helper('shopperapproved')->getEnabled() // Module Status
+                'account' => Mage::helper('shopperapproved')->getAccountId(),
+                'enabled' => Mage::helper('shopperapproved')->getEnabled()
             );
+
+            $customer = Mage::getSingleton('customer/session')->getCustomer();
+            if(!empty($customer)) {
+                    $data['name'] = $customer->getName();
+                    $data['email'] = $customer->getEmail();
+                $address = $customer->getDefaultBillingAddress();
+                if(!empty($address)) {
+                    $address = $customer->getDefaultShippingAddress();
+                }
+                if(!empty($address)) {
+                    $country = Mage::getModel('directory/country')->loadByCode($address->getCountry());
+                    $data['country'] = $country->getName();
+                    $data['state'] = $address->getRegion();
+                }
+            }
+
+            $lastOrderId = Mage::getSingleton('checkout/session')->getLastRealOrderId();
+            if (!empty($lastOrderId)) {
+                $data['orderid'] = $lastOrderId;
+            }
         }
         if(isset($data[$key])) {
             return $data[$key];
--- app/code/community/K3Live/ShopperApproved/Helper/Data.php
+++ app/code/community/K3Live/ShopperApproved/Helper/Data.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * ShopperApproved Module for Magento 
+ * @package     K3Live_ShopperApproved
+ * @author      K3Live (http://www.k3live.com/)
+ * @copyright   Copyright (c) 2014 K3Live (http://www.k3live.com/)
+ * @license     Open Software License
+ */
 
 class K3Live_ShopperApproved_Helper_Data extends Mage_Core_Helper_Abstract
 {
